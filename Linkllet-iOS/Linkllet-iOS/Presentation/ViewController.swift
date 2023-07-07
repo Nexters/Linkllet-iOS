975ae09cd1804ee3f4a8e24c6b49e2138d3f2a17
juhyeon <wngus4296@swu.ac.kr>
2023-07-07 18:24:25 +0900

    [INIT] 프로젝트 생성

---
+++ Linkllet-iOS/Linkllet-iOS/Presentation/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Linkllet-iOS
+//
+//  Created by Juhyeon Byun on 2023/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
