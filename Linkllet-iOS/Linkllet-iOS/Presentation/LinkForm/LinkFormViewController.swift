4129f5e477ee07b3db43e00000dd1f4369fdc904
dochoi <dochoi@student.42seoul.kr>
2023-07-17 20:20:46 +0900

    기본 extension 구현

---
+++ Linkllet-iOS/Linkllet-iOS/Presentation/LinkForm/LinkFormViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LinkFormViewController.swift
+//  Linkllet-iOS
+//
+//  Created by 최동규 on 2023/07/17.
+//
+
+import UIKit
+
+class LinkFormViewController: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
